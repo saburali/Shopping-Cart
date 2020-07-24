4fdccfc2d60589b37b66e0102ec48221a1748c45
Sabur7264 <49846108+Sabur7264@users.noreply.github.com>
2020-07-24 15:54:42 +0600

    Sub price, Tax, Total price Calculation Added

--- custom.js
+++ custom.js
@@ -1,13 +1,23 @@
 const btnAdd = document.querySelector("#plus-btn");
 const btnSub = document.querySelector("#minus-btn");
 const inputValue = document.querySelector('input');
+const price = addFunction("price");
+const subTotal = addFunction("sub-totalPrice");
+const taxPrice = addFunction("tax-price");
+const totalPrice = addFunction("total-price");
 
 // Plus Button Event
 btnAdd.addEventListener('click', function () {
     // Add Number Count
     const valuePlus = inputValue.value = parseInt(inputValue.value) + 1;
     // Calculation Product-rate
-    document.getElementById('price').innerText = (price * valuePlus);
+    const priceValue = document.getElementById('price').innerText = (price * valuePlus);
+    // Sub Total Price
+    const priceResult = document.getElementById('sub-totalPrice').innerText = priceValue;
+    // Tax Price
+    const taxPriceResult = document.getElementById("tax-price").innerText = taxPrice;
+    // Total Price
+    document.getElementById("total-price").innerText = priceResult + taxPriceResult;
 })
 
 // Minus Button Event
@@ -15,19 +25,42 @@ btnSub.addEventListener('click', function () {
     // Sub Number Count Condition
     if (inputValue.value == 0) {
         removeAttribute("desabled")
-    }
-    else {
+    } else {
         // Sub Number Cout
         const valueMinus = inputValue.value = parseInt(inputValue.value) - 1;
         // Calculation Product-rate
-        document.getElementById('price').innerText = (price * valueMinus);
+        const priceValue = document.getElementById('price').innerText = (price * valueMinus);
+        // Sub Total Price
+        const priceResult = document.getElementById('sub-totalPrice').innerText = priceValue;
+        // Tax Price
+        const taxPriceResult = document.getElementById("tax-price").innerText = taxPrice;
+        // Total Price
+        document.getElementById("total-price").innerText = priceResult + taxPriceResult;
     }
 })
 
+
+
 // Price Function
-function addFunction(id){
+function addFunction(id) {
     const productRate = document.getElementById(id).innerText;
     const currentPrice = parseFloat(productRate);
     return currentPrice;
 }
-const price = addFunction("price");
+
+
+
+
+// console.log({
+//     totalPriceResult
+// });
+
+// console.log({
+//     subTotal
+// });
+// console.log({
+//     taxPrice
+// });
+// console.log({
+//     totalPrice
+// });
